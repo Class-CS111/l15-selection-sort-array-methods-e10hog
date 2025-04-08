cb451c7781f6ff481b1b303cee35c6fbc8b8fcb2
Ethan Luong <129918674+e10hog@users.noreply.github.com>
2025-04-08 23:48:15 +0000

    finished array methods

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,8 +1,8 @@
 /********************************************
-*	AUTHOR:	<name>
+*	AUTHOR:	Ethan Luong
 * COLLABORATORS: <names>
 *	COURSE:	CS 111 Intro to CS I - Java
-*	LAST MODIFIED: <date>
+*	LAST MODIFIED: 4/8/25
 ********************************************/
 
 /********************************************
@@ -12,7 +12,7 @@
 *	Collection of useful methods for int arrays.
 *********************************************
 *	ALGORITHM:
-*	TODO: <Pseudocode for selection sort here>
+*	TODO: Find min index and add it to startingIndex, repeat but increase startingIndex by one
 *********************************************
 
 /* UML CLASS DIAGRAM:
@@ -33,31 +33,53 @@ public class ArrayMethods
   /**DESCRIPTION: */
   public static String arrayString(int[] a)
   {
-    return ""; //STUB to keep compiler happy
+    String output = "{";
+    for (int i = 0; i < a.length; i++) {
+      output += a[i];
+      if (i == a.length - 2) break;
+      output += ", ";
+    }
+    return output + "}";
   }
   
   /**DESCRIPTION: */
   public static void swap(int[] array, int a, int b)
   {
-  
+    int aVal = array[a], bVal = array[b];
+    array[a] = bVal;
+    array[b] = aVal;
   }
 	
 	/**DESCRIPTION: */
   public static int indexOfMin(int[] array, int startIndex)
   {
-    return 0; //STUB to keep compiler happy
+    int min = Integer.MAX_VALUE, index = 0;
+    for (int i = startIndex; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+        index = i;
+      }
+    }
+    return index;
   }
 
 	/**DESCRIPTION: */
   public static void reverse(int[] array)
   {
-
+    int times = (int) array.length / 2;
+    for (int i = 0; i < times; i++) {
+      swap(array, i, array.length-i-1);
+    }
   }
 
 	/**DESCRIPTION: */
   public static void selectionSort(int[] array)
   {
-
+    for (int i = 0; i < array.length; i++) {
+      int min = indexOfMin(array, i);
+      if (min == i) continue;
+      swap(array, i, min);;
+    }
   }
 
 }
